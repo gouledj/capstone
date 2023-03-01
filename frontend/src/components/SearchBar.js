b93aec5dc9418d117fed523d7e48893e1db60612
Gouled Jeelal Student <jeelalg@mymacewan.ca>
2023-03-01 09:01:09 -0700

    updated version

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -19,6 +19,8 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Slide from '@mui/material/Slide';
+import Input from '@mui/material/Input';
+
 
 import ViewEditProducts from "/Users/gouledjeelal/Desktop/school/capstone/frontend/src/components/products/ViewEditProduct.js"
 
@@ -31,10 +33,12 @@ const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="down" ref={ref} {...props} />;
 });
 
+const ariaLabel = { 'aria-label': 'description' };
+
 
 //Search bar functionality will handle search but also items that come up below, don't make another component for it.
 //Have an onlick function thats calls another function, and that function sets the state of the props and calls it to another function
-function SearchBar() {
+export default function SearchBar() {
   const [products, setProducts] = useState([]);
   const [load, setLoad] = useState(false);
   const [productClick, setproductClick] = useState(false);
@@ -50,10 +54,11 @@ function SearchBar() {
   const [productWeight, setproductWeight] = useState(null);
   const [productHeight, setproductHeight] = useState(null);
 
-  const [productClickedInfo, setproductClickedInfo] = useState({ productName: 'test', productDescription: 'hellooo' });
+  const [productClickedInfo, setproductClickedInfo] = useState({});
 
+  const [EditInputLabels, setEditInputLabels] = useState(false)
 
-
+  const [buttonClicked, setButtonClicked] = useState(false);
 
 
 
@@ -75,111 +80,133 @@ function SearchBar() {
     setproductClick(false);
   }
 
-  const handleClose = () => {
-    const true_value = true;
-
-    console.log("post values")
-    setOpen(false);
+  //when product is clicked
+  const editFields = () => {
+    setButtonClicked(true);
+    setEditInputLabels(true);
+    console.log("PRESS")
 
   };
 
-  const showproductClick = (props) => {
-    setproductClick(true);
-    console.log(props)
-  }
+  const handleClose = () => {
+    const true_value = true;
+
+    // console.log("post values")
+    // setOpen(false);
+    // setButtonClicked(true);
+    setOpen(false);
+    // setButtonClicked(true);
+    // setEditInputLabels(true);
 
 
+  };
+  const showproductClick = (productClicked) => {
+    const selectedProduct = products[productClicked];
+    console.log(selectedProduct)
+    const imageShortenUrl = selectedProduct.image.substring(selectedProduct.image.lastIndexOf('/') + 1)
+    setproductClickedInfo({
+      productName: selectedProduct.product_name,
+      productDescription: selectedProduct.product_description,
+      productQuantity: selectedProduct.product_quantity,
+      productPrice: selectedProduct.product_price,
+      productPriceSale: selectedProduct.product_price_sale,
+      productWeight: selectedProduct.product_weight,
+      productHeight: selectedProduct.product_height,
+      productImage: imageShortenUrl
+    });
+    setOpen(true);
 
-  const productClickItem = (props) => {
-    console.log(props)
+    // console.log(products[productClicked]);
     return (
+
       <div>
-        {/* <Button variant="contained" onClick={handleClickOpen}>
-          Add Product
+        {/* <Button variant="outlined" onClick={handleClickOpen}>
+          Open form dialog
         </Button> */}
-        <Dialog
-          onClick={handleClickOpen}
-          open={true}
-          TransitionComponent={Transition}
-          keepMounted
-          onClose={handleClose}
-          aria-describedby="alert-dialog-slide-description"
-        >
-          <DialogTitle sx={{ color: "white" }}>{"Fill the required fields below to add a product!"}</DialogTitle>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>Subscribe</DialogTitle>
           <DialogContent>
-            <DialogContentText id="alert-dialog-slide-description">
-              <div className="product-field">
-                <div className="input-fields">
-                  <TextField id="product-name-field" onChange={e => setproductName(e.target.value)} label="Product Name" variant="outlined" />
-                  <TextField id="product-description-field" onChange={e => setproductDescription(e.target.value)} label="Product Description" variant="outlined" />
-                  <TextField id="product-quantity-field" onChange={e => setproductQuantity(e.target.value)} label="Quantity" variant="outlined" />
-                  <TextField id="product-price-field" onChange={e => setproductPrice(e.target.value)} label="Price" variant="outlined" />
-                  <TextField id="product-sale-price-field" onChange={e => setproductPriceSale(e.target.value)} label="Sale Price " variant="outlined" />
-                  <TextField id="product-weight-field" onChange={e => setproductWeight(e.target.value)} label="Weight" variant="outlined" />
-                  <TextField id="product-height-field" onChange={e => setproductHeight(e.target.value)} label="Height" variant="outlined" />
-
-                  {/* {console.log(fileurl)} */}
-
-                </div>
-
-              </div>
-
+            <DialogContentText>
+              To subscribe to this website, please enter your email address here. We
+              will send updates occasionally.
             </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Email Address"
+              type="email"
+              fullWidth
+              variant="standard"
+            />
           </DialogContent>
           <DialogActions>
-            <Button onClick={exitProductAdd}>Exit</Button>
-            <Button onClick={handleClose}>Add Product</Button>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={handleClose}>Subscribe</Button>
           </DialogActions>
         </Dialog>
       </div>
     );
-
   }
 
-  console.log(products)
-  return (
 
+  // console.log(products)
+  return (
     <div className="item-gallery-box">
       <div className="search-bar">
         <Autocomplete
           disablePortal
           id="combo-box-demo"
-          options={top100Films}
           sx={{ width: '1240px', backgroundColor: '#ECEBF1', borderRadius: '3rem' }}
           renderInput={(params) => <TextField {...params} label="Search" />}
         />
-
       </div>
 
       <div className='item-flex'>
         {products.map((item, id) => (
-
-          <div onClick={showproductClick(item.product_name)} key={id} className="product-details-list">
-
+          <div onClick={() => showproductClick(id)} key={id} className="product-details-list">
             <div className="product-name">{item.product_name}</div>
-            <div className="product-description"> {item.product_description}</div>
+            {/* <div className="product-description"> {item.product_description}</div> */}
             <img className="product-img" src={require('/src/productimages/' + item.image.substring(item.image.lastIndexOf('/') + 1))} />
-            <div className="product-price">{item.product_price}</div>
-            <div className="product-ships">{item.product_height}</div>
-            <div className="product-avaliable">{item.product_available === "false" ? "false" : "true"}</div>
-
-
+            <div className="product-price">{item.product_price + "$"}</div>
+            {/* <div className="product-ships">{item.product_height}</div> */}
+            <div className="product-avaliable">{item.product_available === "false" ? "true" : "Avaliable "}</div>
           </div>
-
-
-
         ))}
       </div>
 
+      <Dialog className='product-click-window' open={open} onClose={handleClose}>
+        <DialogContent>
+          {productClickedInfo.productImage ? (
+            <img className="product-clicked-img" src={require('/src/productimages/' + productClickedInfo?.productImage)} />
+          ) : null}
 
+          <div className="product-details">
+
+            <Input disabled={!EditInputLabels} fullWidth={true} disableUnderline={true} placeholder="Product Name" defaultValue={"Product Name: " + productClickedInfo.productName} />
+            <Input disabled={!EditInputLabels} fullWidth={true} disableUnderline={true} placeholder="Price" defaultValue={"Product Price: " + productClickedInfo.productPrice + "$"} />
+            <Input disabled={!EditInputLabels} fullWidth={true} disableUnderline={true} placeholder="Dimensions" defaultValue={"Product Dimensions: " + productClickedInfo.productHeight + "x" + productClickedInfo.productWidth} />
+            <Input disabled={!EditInputLabels} fullWidth={true} disableUnderline={true} placeholder="Weight" defaultValue={"Product Weight: " + productClickedInfo.productWeight} />
+            <Input disabled={!EditInputLabels} fullWidth={true} disableUnderline={true} placeholder="Quantity" defaultValue={"Product Quantity: " + productClickedInfo.productQuantity} />
+          </div>
+          <h3 className="product-description-title">Product Description</h3>
+          <Input disabled={!EditInputLabels} disableUnderline={true} multiline={true} placeholder="Product Description" defaultValue={productClickedInfo.productDescription} inputProps={ariaLabel} />
+        </DialogContent>
+        <DialogActions>
+          {/* <Button onClick={handleClose}>
+            {buttonClicked ? "Changes Made" : "Make Changes"}
+          </Button> */}
+          {/* {buttonClicked ? "Changes Made" : "Make Changes"} */}
+          <Button className="edit-window-button" variant="contained" onClick={editFields}>
+            {/* {buttonClicked ?
+              <Button className="new-test" />
+            
+          } : null} */}
+            {buttonClicked ? "Save Changes" : "Edit Product Info"}
+          </Button>
+
+        </DialogActions>
+      </Dialog>
     </div>
   )
 }
-
-const top100Films = [
-  { label: 'The Shawshank Redemption', year: 1994 },
-  { label: 'The Godfather', year: 1972 },
-  { label: 'The Godfather: Part II', year: 1974 },
-]
-
-export default SearchBar
--- store/settings.py
+++ store/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'api.apps.ApiConfig',
     'rest_framework',
     'corsheaders',
+    'rest_framework_simplejwt.token_blacklist',
 ]
 
 MIDDLEWARE = [
@@ -53,6 +55,19 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+      ],
+}
+
+SIMPLE_JWT = {
+     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
+     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+     'ROTATE_REFRESH_TOKENS': True,
+     'BLACKLIST_AFTER_ROTATION': True
+}
 
 CORS_ORIGIN_ALLOW_ALL = True
 
